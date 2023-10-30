43c1a190b39919355508e9d6a85668d42dfe44cf
wenlao peng <s3690691@student.rmit.edu.au>
2023-10-30 17:08:06 +1100

    Update button to roll dice

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,5 +1,6 @@
-import 'package:first_app/style_text.dart';
 import 'package:flutter/material.dart';
+
+import 'package:first_app/dice_roller.dart';
 
 const startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
@@ -7,10 +8,12 @@ const endAlignment = Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
 
+  const GradientContainer.purple({super.key})
+      : color1 = const Color.fromARGB(255, 71, 9, 177),
+        color2 = const Color.fromARGB(255, 40, 5, 101);
+
   final Color color1;
   final Color color2;
-
-  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -22,26 +25,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-2.png',
-              width: 200,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextButton(
-                onPressed: rollDice,
-                style: TextButton.styleFrom(
-                    // padding: const EdgeInsets.all(20),
-                    foregroundColor: Colors.white,
-                    textStyle: const TextStyle(fontSize: 28)),
-                child: const Text('Roll Text')),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
