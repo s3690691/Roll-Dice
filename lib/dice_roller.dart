a0c67c50c8cb1c2dfc45fef125a7101a54db3b1b
wenlao peng <s3690691@student.rmit.edu.au>
2023-11-02 12:17:24 +1100

    Finish roll dice

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+final randomizer = Random();
 
 class DiceRoller extends StatefulWidget {
   const DiceRoller({super.key});
@@ -9,11 +12,11 @@ class DiceRoller extends StatefulWidget {
 }
 
 class _DiceRollerState extends State<DiceRoller> {
-  var activeDiceImage = 'assets/images/dice-2.png';
+  var currentDiceRoll = 2;
 
   void rollDice() {
     setState(() {
-      activeDiceImage = 'assets/images/dice-4.png';
+      currentDiceRoll = randomizer.nextInt(6) + 1;
     });
   }
 
@@ -23,7 +26,7 @@ class _DiceRollerState extends State<DiceRoller> {
       mainAxisSize: MainAxisSize.min,
       children: [
         Image.asset(
-          activeDiceImage,
+          'assets/images/dice-$currentDiceRoll.png',
           width: 200,
         ),
         const SizedBox(
@@ -35,7 +38,7 @@ class _DiceRollerState extends State<DiceRoller> {
                 // padding: const EdgeInsets.all(20),
                 foregroundColor: Colors.white,
                 textStyle: const TextStyle(fontSize: 28)),
-            child: const Text('Roll Text')),
+            child: const Text('Roll Dice')),
       ],
     );
   }
